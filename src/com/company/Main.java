2e805039e352923f5b9fd87f0b2964f7c48127e8
s542367 <s542367@nwmissouri.edu>
2022-06-21 09:42:17 -0500

    second commit to local master repo without circle class

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,9 +1,84 @@
 package com.company;
 
+abstract class Shape {
+    String color;
+
+    // these are abstract methods
+    abstract double area();
+    public abstract String toString();
+
+    // abstract class can have the constructor
+    public Shape(String color)
+    {
+        System.out.println("Shape constructor called");
+        this.color = color;
+    }
+
+    // this is a concrete method
+    public String getColor() { return color; }
+}
+
+class Circle extends Shape {
+    double radius;
+
+    public Circle(String color, double radius)
+    {
+
+        // calling Shape constructor
+        super(color);
+        System.out.println("Circle constructor called");
+        this.radius = radius;
+    }
+    @Override double area()
+    {
+        return Math.PI * Math.pow(radius, 2);
+    }
+
+    @Override public String toString()
+    {
+        return "Circle color is " + super.getColor()
+                + " and area is : " + area();
+    }
+}
+
+
+// this is the only rectangle class in the main file now
+class Rectangle extends Shape {
+
+    double length;
+    double width;
+
+    public Rectangle(String color, double length,
+                     double width)
+    {
+        // calling Shape constructor
+        super(color);
+        System.out.println("Rectangle constructor called");
+        this.length = length;
+        this.width = width;
+    }
+    @Override double area() { return length * width; }
+
+    @Override public String toString()
+    {
+        return "Rectangle color is " + super.getColor()
+                + "and area is : " + area();
+    }
+}
+
+
+
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        System.out.println("God morning my dear !!!!");
+
+                 Shape s1 = new Circle("Red", 2.2);
+                Shape s2 = new Rectangle("Yellow", 2, 4);
+
+                // System.out.println(s1.toString());
+                System.out.println(s2.toString());
+
     }
 }
