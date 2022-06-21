12501d0cf26ee58825d100b9ee371ea813827a6e
s542367 <s542367@nwmissouri.edu>
2022-06-21 09:46:55 -0500

    second commit to local feature repo with a for loop.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,7 +3,11 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        // write your code here
         System.out.println("God morning my dear !!!!");
+
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(i);
+        }
     }
 }
